6485c733a7fb69d54caa49cac626133b19c8a0ea
Mark <mkinoshita@csumb.edu>
2017-08-30 19:37:18 +0000

    commit

---
+++ PersonalPortfolio/index.php
@@ -0,0 +1,68 @@
+
+<html>
+<!--
+
+Mark Kinoshita 
+CST 336:Lab 1
+
+
+-->
+
+<!-- This is the head -->
+<!-- All styles and javascript go inside the head -->
+    <head>
+        <meta charset="utf-8" />
+        <title> Mark Kinoshita: Personal Website</title>
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+    </head>
+<?php
+
+?>
+    <body>
+        <header>
+            <h1> Mark Kinoshita </h1>
+        </header>
+        <nav>
+            <hr width="50%" />
+            <a href="index.php"><em>Home</em></a>
+            <a href="about.html">About</a>
+            <a href="contact.html">Contact</a>
+        </nav>
+        
+        <br /><br />
+        
+        <main>
+            <figure id="me">
+                <img src="img/profile.png" alt="Picture of Juan Doe"/>
+            </figure>
+            
+            <div id="welcomeText">
+                Hello! <br />
+                <p>Thank you for visiting my professional portfolio website.</p>
+            
+                <p>I am a Software Engineer and have been recently hired at the Acme Corporation.</p>
+                
+                <p>Feel free to contact me!</p>
+                
+                <br /><br />
+                
+                <em>"With ordinary talent and extraordinary <strong>perserveracne</strong>, all things are attainable"</em><br/>
+                - Thomas F. Buxton
+                
+            </div>
+        </main>
+       
+        <footer>
+            <hr>
+            <img id ="logo" src="img/logo.png" alt="Picture of CSUMB Logo"/> <br />
+            Course Name. 2017&copy; Kinoshita <br />
+            <strong>Disclaimer:</strong> The information in this webpage is fictitous. <br />
+            It is used for academic purposes only. 
+        </footer>
+        <!-- closing footer -->
+        
+    </body>
+    <!-- closing body -->
+
+</html>
